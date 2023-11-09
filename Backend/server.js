65df1b8c9a4a61bd19799598da0da6f419d3dd1a
judiciousmurich <murichjudicious45@gmail.com>
2023-11-09 11:39:32 +0300

    feat:add server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import mysql from 'mysql';
-import config from './config/config.js'; // Your database configuration
+import config from './config/config.js'; 
 
 const app = express();
 const pool = mysql.createPool(config.mysql);
 
-// Your routes and controllers can be imported and used here
 
-const PORT = 8081; // Use the desired port, such as 8081
+
+const PORT = 8081; 
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
