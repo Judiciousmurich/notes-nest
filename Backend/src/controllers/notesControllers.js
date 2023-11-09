5afd9dc01bfd05256e4319464e77b2021b58936d
judiciousmurich <murichjudicious45@gmail.com>
2023-11-09 11:27:32 +0300

    feat:add notesController

--- Backend/src/controllers/notesControllers.js
+++ Backend/src/controllers/notesControllers.js
@@ -1,38 +1,44 @@
-const mysql = require('mysql');
-const config = require('../config/config.js');
+import mysql from 'mysql';
+import config from '../config/config.js';
 
 const pool = mysql.createPool(config.mysql);
 
 // Get all notes
-const getNotes = (req, res) => {
-  pool.query('SELECT * FROM Notes', (error, results) => {
+export const getNotes = (req, res) => {
+  pool.query('SELECT * FROM notes', (error, results) => {
     if (error) {
       res.status(500).json({ error: `An error occurred while retrieving notes... ${error.message}` });
     } else {
-      res.status(200).json(results);
+      if (results.length === 0) {
+        res.status(404).json({ message: 'Notes not found' });
+      } else {
+        res.status(200).json(results);
+      }
     }
   });
 };
 
 // Get a specific note by ID
-const getNoteById = (req, res) => {
+export const getNoteById = (req, res) => {
   const noteId = req.params.id;
-  pool.query('SELECT * FROM Notes WHERE id = ?', noteId, (error, results) => {
+  pool.query('SELECT * FROM notes WHERE id = ?', noteId, (error, results) => {
     if (error) {
       res.status(500).json({ error: `An error occurred while retrieving the note... ${error.message}` });
-    } else if (results.length === 0) {
-      res.status(404).json({ message: 'Note not found' });
     } else {
-      res.status(200).json(results[0]);
+      if (results.length === 0) {
+        res.status(404).json({ message: 'Note not found' });
+      } else {
+        res.status(200).json(results[0]);
+      }
     }
   });
 };
 
 // Create a new note
-const createNote = (req, res) => {
+export const createNote = (req, res) => {
   const { title, content } = req.body;
   const created_at = new Date().toISOString().slice(0, 19).replace('T', ' ');
-  pool.query('INSERT INTO Notes (title, content, created_at) VALUES (?, ?, ?)', [title, content, created_at], (error) => {
+  pool.query('INSERT INTO notes (title, content, created_at) VALUES (?, ?, ?)', [title, content, created_at], (error) => {
     if (error) {
       res.status(500).json({ error: `An error occurred while creating the note... ${error.message}` });
     } else {
@@ -42,10 +48,10 @@ const createNote = (req, res) => {
 };
 
 // Update a note by ID
-const updateNote = (req, res) => {
+export const updateNote = (req, res) => {
   const noteId = req.params.id;
   const { title, content } = req.body;
-  pool.query('UPDATE Notes SET title = ?, content = ? WHERE id = ?', [title, content, noteId], (error) => {
+  pool.query('UPDATE notes SET title = ?, content = ? WHERE id = ?', [title, content, noteId], (error) => {
     if (error) {
       res.status(500).json({ error: `An error occurred while updating the note... ${error.message}` });
     } else {
@@ -55,9 +61,9 @@ const updateNote = (req, res) => {
 };
 
 // Delete a note by ID
-const deleteNote = (req, res) => {
+export const deleteNote = (req, res) => {
   const noteId = req.params.id;
-  pool.query('DELETE FROM Notes WHERE id = ?', noteId, (error) => {
+  pool.query('DELETE FROM notes WHERE id = ?', noteId, (error) => {
     if (error) {
       res.status(500).json({ error: `An error occurred while deleting the note... ${error.message}` });
     } else {
@@ -65,5 +71,3 @@ const deleteNote = (req, res) => {
     }
   });
 };
-
-
