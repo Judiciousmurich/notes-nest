6eb8b4031e9801da14e8747120e6a735e816b237
judiciousmurich <murichjudicious45@gmail.com>
2023-11-13 14:50:21 +0300

    feat:add Tagging

---
+++ Client/src/pages/Tagging.jsx
@@ -0,0 +1,20 @@
+// Tagging.jsx
+import React from 'react';
+
+const Tagging = ({ onAddTag, onDeleteTag, tags }) => {
+  return (
+    <div>
+      <h2>Tags</h2>
+      <ul>
+        {tags.map((tag) => (
+          <li key={tag}>
+            {tag} <button onClick={() => onDeleteTag(tag)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <input type="text" placeholder="Add a tag..." onKeyDown={(e) => e.key === 'Enter' && onAddTag(e.target.value)} />
+    </div>
+  );
+};
+
+export default Tagging;
