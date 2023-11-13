1c80234622b5905ddf7c71f5c2c5c08c2ba283ed
judiciousmurich <murichjudicious45@gmail.com>
2023-11-13 14:54:40 +0300

    feat:add Auth

---
+++ Client/src/pages/Auth.jsx
@@ -0,0 +1,19 @@
+// Auth.jsx
+import React from 'react';
+
+const Auth = ({ onLogin, onRegister, onLogout, user }) => {
+  return (
+    <div>
+      {user ? (
+        <button onClick={onLogout}>Logout</button>
+      ) : (
+        <>
+          <button onClick={onLogin}>Login</button>
+          <button onClick={onRegister}>Register</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Auth;
