c8afbf823bdd2d88af12e6f2e1bf0918e3b76dd0
judiciousmurich <murichjudicious45@gmail.com>
2023-11-13 14:45:25 +0300

    feat:add Header

---
+++ Client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+// Header.jsx
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white p-4">
+      <h1 className="text-xl font-semibold">Notes App</h1>
+    </header>
+  );
+};
+
+export default Header;
