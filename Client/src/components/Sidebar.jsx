7c340acbed6fd40a9cc4cb8a933268960eab2219
judiciousmurich <murichjudicious45@gmail.com>
2023-11-13 14:31:25 +0300

    feat:add Sidebar

--- Client/src/components/Sidebar.jsx
+++ Client/src/components/Sidebar.jsx
@@ -1,83 +1,35 @@
-const Sidebar = ({
-    notes,
-    onAddNote,
-    onDeleteNote,
-    activeNote,
-    setActiveNote,
-  }) => {
-    const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
-  
-    return (
-      <div className="app-sidebar">
+/* Add this to your styles */
+@import url("https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css");
+
+/* Add these classes to your HTML elements */
+<div className="bg-gradient-to-r from-green-400 to-blue-500">
+
+    <button className="hover:text-blue-500">Hover me</button>
+
+    <div className="app-sidebar">
         <div className="app-sidebar-header">
-          <h1>Notes</h1>
-          <button onClick={onAddNote}>Add</button>
+            <h1 className="text-2xl font-bold mb-4">Notes</h1>
+            <button
+                className="bg-transparent hover:bg-blue-500 text-blue-700 hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                onClick={onAddNote}
+            >
+                Add
+            </button>
         </div>
         <div className="app-sidebar-notes">
-          {sortedNotes.map(({ id, title, body, lastModified }, i) => (
-            <div
-              className={`app-sidebar-note ${id === activeNote && "active"}`}
-              onClick={() => setActiveNote(id)}
-            >
-              <div className="sidebar-note-title">
-                <strong>{title}</strong>
-                <button onClick={(e) => onDeleteNote(id)}>Delete</button>
-              </div>
-  
-              <p>{body && body.substr(0, 100) + "..."}</p>
-              <small className="note-meta">
-                Last Modified{" "}
-                {new Date(lastModified).toLocaleDateString("en-GB", {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
-              </small>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  };
-  
-  export default Sidebar;const Sidebar = ({
-  notes,
-  onAddNote,
-  onDeleteNote,
-  activeNote,
-  setActiveNote,
-}) => {
-  const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
+            {sortedNotes.map(({ id, title, body, lastModified }, i) => (
+                <div
+                    className={`app-sidebar-note ${id === activeNote && "active"}`}
+                    onClick={() => setActiveNote(id)}
+                >
+                    <div className="sidebar-note-title">
+                        <strong>{title}</strong>
+                        <button
+                            className="text-red-600 hover:text-red-400"
+                            onClick={(e) => onDeleteNote(id)}
+                        >
+                            Delete
+                        </button>
+                    </div>
 
-  return (
-    <div className="app-sidebar">
-      <div className="app-sidebar-header">
-        <h1>Notes</h1>
-        <button onClick={onAddNote}>Add</button>
-      </div>
-      <div className="app-sidebar-notes">
-        {sortedNotes.map(({ id, title, body, lastModified }, i) => (
-          <div
-            className={`app-sidebar-note ${id === activeNote && "active"}`}
-            onClick={() => setActiveNote(id)}
-          >
-            <div className="sidebar-note-title">
-              <strong>{title}</strong>
-              <button onClick={(e) => onDeleteNote(id)}>Delete</button>
-            </div>
-
-            <p>{body && body.substr(0, 100) + "..."}</p>
-            <small className="note-meta">
-              Last Modified{" "}
-              {new Date(lastModified).toLocaleDateString("en-GB", {
-                hour: "2-digit",
-                minute: "2-digit",
-              })}
-            </small>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default Sidebar;
+                    <p>{body && body.substr(0
