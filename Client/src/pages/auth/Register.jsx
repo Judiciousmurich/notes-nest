fac5f65fc3b3d303d85db278f7591c30b56846ff
judiciousmurich <murichjudicious45@gmail.com>
2023-11-14 23:05:30 +0300

    feat:add register

---
+++ Client/src/pages/auth/Register.jsx
@@ -0,0 +1,18 @@
+
+const Auth = ({ onLogin, onRegister, onLogout, user }) => {
+    return (
+      <div>
+        {user ? (
+          <button onClick={onLogout}>Logout</button>
+        ) : (
+          <>
+            <button onClick={onLogin}>Login</button>
+            <button onClick={onRegister}>Register</button>
+          </>
+        )}
+      </div>
+    );
+  };
+  
+  export default Auth;
+  
