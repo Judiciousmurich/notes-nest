e78d4d6321d36fd66d4e7d8d82695909f066b647
judiciousmurich <murichjudicious45@gmail.com>
2023-11-16 21:34:51 +0300

    feat:add routes

--- Client/src/routes/Route.jsx
+++ Client/src/routes/Route.jsx
@@ -2,7 +2,13 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
-                 {/* USER ROUTES*/}
-                 </Route>
-                     )
-                     );
+            {/* USER ROUTES*/}
+        </Route>
+         {/* AUTH ROUTES */}
+         <Route>
+         <Route path="auth" element={<AuthLayout />}>
+         <Route path="login" element={<Login />} />
+         <Route path="signup" element={<Signup />} />
+     </Route>
+    )
+);
