bf53e22a3d47f9117eb0b57183a00f452613b200
judiciousmurich <murichjudicious45@gmail.com>
2023-11-20 22:00:53 +0300

    feat:add notes filter

--- Client/src/pages/Notes.jsx
+++ Client/src/pages/Notes.jsx
@@ -2,23 +2,51 @@ import React, { useState } from 'react';
 
 const Notes = () => {
   const [notes, setNotes] = useState([]);
-
   const [currentNote, setCurrentNote] = useState('');
+  const [currentCategory, setCurrentCategory] = useState(''); // New state for the selected category
 
   const addNote = () => {
-    if (currentNote.trim() !== '') {
-      setNotes([...notes, currentNote]);
+    if (currentNote.trim() !== '' && currentCategory.trim() !== '') {
+      // Include category information with the note
+      setNotes([...notes, { note: currentNote, category: currentCategory }]);
       setCurrentNote('');
+      setCurrentCategory('');
     }
   };
+
+  const filterNotes = (category) => {
+    setCurrentCategory(category);
+  };
+
+  // Filtered notes based on the selected category
+  const filteredNotes = currentCategory
+    ? notes.filter((note) => note.category === currentCategory)
+    : notes;
 
   return (
     <div>
       <h1>My Notes</h1>
 
+      {/* Category Filter */}
+      <div>
+        <label>Select Category: </label>
+        <select
+          value={currentCategory}
+          onChange={(e) => filterNotes(e.target.value)}
+        >
+          <option value="">All</option>
+          {/* Add your specific categories here */}
+          <option value="work">Work</option>
+          <option value="personal">Personal</option>
+          {/* Add more categories as needed */}
+        </select>
+      </div>
+
       <ul>
-        {notes.map((note, index) => (
-          <li key={index}>{note}</li>
+        {filteredNotes.map((note, index) => (
+          <li key={index}>
+            {note.note} - Category: {note.category}
+          </li>
         ))}
       </ul>
 
@@ -29,6 +57,17 @@ const Notes = () => {
           onChange={(e) => setCurrentNote(e.target.value)}
           placeholder="Enter your note"
         />
+        <label>Select Category: </label>
+        <select
+          value={currentCategory}
+          onChange={(e) => setCurrentCategory(e.target.value)}
+        >
+          {/* Add your specific categories here */}
+          <option value="">Select</option>
+          <option value="work">Work</option>
+          <option value="personal">Personal</option>
+          {/* Add more categories as needed */}
+        </select>
         <button onClick={addNote}>Add Note</button>
       </div>
     </div>
