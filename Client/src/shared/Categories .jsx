cfc712607e055c2ce0b67556090c180e219073a9
judiciousmurich <murichjudicious45@gmail.com>
2023-11-21 22:04:15 +0300

    feat:add categories

---
+++ Client/src/shared/Categories .jsx
@@ -0,0 +1,13 @@
+const uniqueCategories = Array.from(new Set(notes.map(note => note.category)));
+
+// ...
+
+<select
+  value={currentCategory}
+  onChange={(e) => filterNotes(e.target.value)}
+>
+  <option value="">All</option>
+  {uniqueCategories.map((category, index) => (
+    <option key={index} value={category}>{category}</option>
+  ))}
+</select>
